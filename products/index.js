983b7d2d128f59c5a3b23ec2c5f641c79a162819
Rodolfo Oliveira <rmgoliveira97@gmail.com>
2023-07-07 22:51:29 +0100

    new-product-end-point-issue-resolve-#4

--- products/index.js
+++ products/index.js
@@ -1,1 +1,23 @@
-module.exports = {}
+const express = require("express");
+const router = express.Router();
+const { products } = require("../db");
+
+router.post("/", (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.category || !product.price || !product.image) {
+    return res.status(400).json({ error: "Invalid product" });
+  }
+
+  const newProduct = {
+    id: products.length + 1,
+    name: product.name,
+    category: product.category,
+    price: product.price,
+    image: product.image,
+  };
+  products.push(newProduct);
+
+  res.status(201).json(newProduct);
+});
+
+module.exports = router;
