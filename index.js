93434b85991448925495707eb0609ad6cdaf3130
Diogo Simoes <diogo@cloudoki.com>
2023-07-07 20:42:46 +0100

    issue-8: get user by id

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const { logger } = require("./middleware");
+const users = require("./users");
 
 app.use(logger);
+
+app.use("/users", users);
 
 app.use((err, req, res, next) => {
     console.error(err);
--- users/index.js
+++ users/index.js
@@ -1,1 +1,12 @@
-module.exports = {}
+const express = express()
+const router = express.Router()
+const db = require("../db")
+
+// get by id
+router.get("/:id", (req, res) => {
+    const user = db.users.find((u) => u.id === req.params.id)
+    if(!user) return res.status(404).json({ error: "user not found" })
+    res.status(200).json(user)
+})
+
+module.exports = router
