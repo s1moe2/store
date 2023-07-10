69ce78285057ab8b45d21fa9b6fe3c309be53f98
Diogo Simoes <diogo@cloudoki.com>
2023-07-10 20:09:11 +0100

    feat: update user

--- users/index.js
+++ users/index.js
@@ -1,36 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const { users } = require("../db");
 
 // get by id
 router.get("/:id", (req, res) => {
-  const user = db.users.find((u) => u.id === parseInt(req.params.id));
-  console.log(user);
+  const user = users.find((u) => u.id === parseInt(req.params.id));
   if (!user) return res.status(404).json({ error: "user not found" });
   res.status(200).json(user);
 });
 
 router.put("/:id", (req, res) => {
   //find user by id
-  const user = db.users.find((u) => u.id === parseInt(req.params.id));
+  const userIx = users.findIndex((u) => u.id === parseInt(req.params.id));
 
   //user exists?
-
-  const body = req.body;
-  const name = body.name;
-  const email = body.email;
-
-  if (user) {
-    //update
-    user.name = name;
-    user.email = email;
-    console.log(user);
-    
-    //result
-    res.status(200).json(user);
-  } else {
+  if (userIx === -1) {
     return res.status(404).json({ error: "User not found" });
   }
+
+  //update
+  users[userIx].name = req.body.name;
+  users[userIx].email = req.body.email;
+
+  //result
+  res.status(200).json(user);
 });
 
 module.exports = router;
