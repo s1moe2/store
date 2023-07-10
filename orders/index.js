2986dbad5f0d52bc84c1ed0c46f888af26cffc3e
joelhmalmeida <joelhmalmeida@gmail.com>
2023-07-10 22:53:08 +0100

    issue-7: validation  order status

--- orders/index.js
+++ orders/index.js
@@ -8,8 +8,14 @@ router.put("/:id", (req, res) => {
   );
   //order exists
   if (orderIx === -1) return res.status(404).json({ error: "order not found" });
+
   //order update
   orders[orderIx].status = req.body.status;
+
+  const possibleStatus = ["created", "preparing", "shipped", "delivered"];
+  if (!possibleStatus.includes(orders[orderIx].status)) {
+    return res.status(400).json({ error: "Invalid body" });
+  }
   //result
   res.status(200).json(orders);
 });
