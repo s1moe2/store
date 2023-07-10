64038c9e349b97135e63d428db30a465b5a546cf
Diogo Simoes <diogo@cloudoki.com>
2023-07-10 19:58:44 +0100

    feat: filter by category

--- products/index.js
+++ products/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { products } = require("../db");
 
 router.get("/", (req, res) => {
+  const products = products.filter((p) => p.category === req.query.cat);
+
   res.status(200).json(products);
 });
 
