e62ef5f23a8051c354ff6edcf05d8b975921b514
AndreiaMsT <am.teixeira4@gmail.com>
2023-07-10 16:13:39 +0100

    users name anda email updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const { logger } = require("./middleware");
 const users = require("./users");
 
@@ -8,10 +9,10 @@ app.use(logger);
 app.use("/users", users);
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: "internal error" });
+  console.error(err);
+  res.status(500).json({ error: "internal error" });
 });
 
 app.listen(3000, () => {
-    console.log("engine started...");
+  console.log("engine started...");
 });
--- users/index.js
+++ users/index.js
@@ -1,12 +1,36 @@
-const express = require("express")
-const router = express.Router()
-const db = require("../db")
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
 
 // get by id
 router.get("/:id", (req, res) => {
-    const user = db.users.find((u) => u.id === req.params.id)
-    if(!user) return res.status(404).json({ error: "user not found" })
-    res.status(200).json(user)
-})
+  const user = db.users.find((u) => u.id === parseInt(req.params.id));
+  console.log(user);
+  if (!user) return res.status(404).json({ error: "user not found" });
+  res.status(200).json(user);
+});
 
-module.exports = router
+router.put("/:id", (req, res) => {
+  //find user by id
+  const user = db.users.find((u) => u.id === parseInt(req.params.id));
+
+  //user exists?
+
+  const body = req.body;
+  const name = body.name;
+  const email = body.email;
+
+  if (user) {
+    //update
+    user.name = name;
+    user.email = email;
+    console.log(user);
+    
+    //result
+    res.status(200).json(user);
+  } else {
+    return res.status(404).json({ error: "User not found" });
+  }
+});
+
+module.exports = router;
