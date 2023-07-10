1f968cce53d8c2fc8ed889c61901892cfcf1617f
joelhmalmeida <joelhmalmeida@gmail.com>
2023-07-10 22:24:05 +0100

    issue7: update order status

--- orders/index.js
+++ orders/index.js
@@ -1,1 +1,19 @@
-module.exports = {}
+const express = require("express");
+const router = express.Router();
+const { orders } = require("../db");
+
+router.put("/:id", (req, res) => {
+  const orderIx = orders.findIndex(
+    (order) => order.id === parseInt(req.params.id)
+  );
+  //order exists
+  if (orderIx === -1) return res.status(404).json({ error: "order not found" });
+  //order update
+  orders[orderIx].status = req.body.status;
+  //result
+  res.status(200).json(orders);
+});
+
+module.exports = router;
+
+// created, preparing, shipped, delivered
