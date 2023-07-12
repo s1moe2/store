2ebbe904d02aa0aa439c1db42d0395d0bb2d2405
sanchopedro <pedro_sancho@yahoo.com.br>
2023-07-12 22:52:11 +0100

    issue#11: Add customer reward points on sales

--- db/index.js
+++ db/index.js
@@ -12,22 +12,6 @@ const db = {
           image:
             "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_PT",
         },
-        {
-          id: 2,
-          name: "Macbook dasdadasd",
-          category: "tech",
-          price: 3000,
-          image:
-            "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_PT",
-        },
-        {
-          id: 2,
-          name: "Macbook dasdadasd",
-          category: "tech",
-          price: 3000,
-          image:
-            "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_PT",
-        },
       ],
       price: 3000,
       orderedAt: new Date(2023, 0, 1, 10, 0, 0),
@@ -39,7 +23,7 @@ const db = {
       id: 1,
       name: "Bill Gates",
       email: "bg@ms.x",
-      spent: 3000,
+      spent: 3000
     },
   ],
   products: [
--- orders/index.js
+++ orders/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { orders } = require("../db");
+const { orders, users } = require("../db");
+const { rewardsPoints } = require("./rewardspoints");
 
 router.put("/:id", (req, res) => {
   const orderIx = orders.findIndex((order) => order.id === parseInt(req.params.id));
@@ -27,20 +28,39 @@ router.post("/", (req, res) => {
 
   const status = "placed"; // Assuming "placed" is a valid status
 
+  const finalPrice = calculateTotalPrice(products);
+
   // criar o objeto order
   const order = {
     id: generateOrderId(),
     userId,
     products,
-    price: calculateTotalPrice(products),
+    price: finalPrice,
     dateTime: new Date(),
     status,
+    rewardPoints: rewardsPoints(finalPrice),
   };
 
   orders.push(order);
 
+  // update user reward pts
+
+  const userIx = users.findIndex((u) => u.id === parseInt(order.userId));
+
+  if (userIx === -1) return res.status(404).json({ error: "user not found" });
+
+  const user = users[userIx];
+
+  if (!user.rewardPoints) {
+    users[userIx].rewardPoints = 0;
+  }
+
+  const userPoint = user.rewardPoints + order.rewardPoints;
+
+  users[userIx].rewardPoints = userPoint;
+
   // enviar resposta
-  res.status(200).json(order);
+  res.status(200).json({ order, users });
 });
 
 // funcao calcular total produto
@@ -49,7 +69,7 @@ function calculateTotalPrice(products) {
   for (let i = 0; i < products.length; i++) {
     totalPrice += products[i].price;
   }
-  return totalPrice;
+  return totalPrice.toFixed(2);
 }
 
 // Gerar um id aleatorio
