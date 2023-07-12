47ce7b21c17fa2a4b1a36ae7bdbb675a0b6b98ef
AndreiaMsT <am.teixeira4@gmail.com>
2023-07-12 18:28:25 +0100

    top X products listed

--- products/index.js
+++ products/index.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const { products } = require("../db");
+const { orders } = require("../db");
 
-router.get("/", (req, res) => {
-  const products = products.filter((p) => p.category === req.query.cat);
+// router.get("/", (req, res) => {
+//   const prd = products.filter((p) => p.category === req.query.cat);
 
-  res.status(200).json(products);
-});
+//   res.status(200).json(prd);
+// });
 
-router.get("/:id", (req, res, next) => {
-  try {
-    const products = products.find((product) => product.id === parseInt(req.params.id));
-    res.status(200).json(products);
-  } catch (err) {
-    next(err);
+// router.get("/:id", (req, res, next) => {
+//   try {
+//     const prd = products.find(
+//       (product) => product.id === parseInt(req.params.id)
+//     );
+//     res.status(200).json(prd);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+router.get("/bestsellers", (req, res) => {
+  const bestsellers = [];
+
+  orders.forEach((order) => {
+    order.products.forEach((product) => {
+      const productIndex = bestsellers.findIndex((p) => {
+        return p.id === product.id;
+      });
+
+      if (productIndex === -1) {
+        bestsellers.push({ id: product.id, name: product.name, count: 1 });
+      } else {
+        bestsellers[productIndex].count += 1;
+      }
+    });
+  });
+
+  const { top } = req.query;
+
+  if (top <= 0 || isNaN(top)) {
+    return res.status(400).json({ error: "invalid value" });
   }
+  const topProducts = bestsellers
+    .sort((a, b) => b.count - a.count)
+    .slice(0, top);
+
+  return res.status(200).json(topProducts);
 });
 
 router.delete("/:id", (req, res) => {
@@ -24,7 +56,7 @@ router.delete("/:id", (req, res) => {
   }
 
   products.splice(productIx, 1);
-  res.status(200).json({ message: 'Product removed successfully.' });
+  res.status(200).json({ message: "Product removed successfully." });
 });
 
 router.post("/", (req, res) => {
