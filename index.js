d0bf37b4df6be0bd885ac2eb58ceeffff8d1e625
Catarina Mendes <127792100+CatarinaFilipaMendes@users.noreply.github.com>
2023-07-12 19:25:41 +0100

    issue6

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: "internal error" });
 });
 
+app.get("/users", (req, res) => {
+    const users = db.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      };
+    });
+
+    res.json(users);
+    console.log(users);
+  });
+
 app.listen(3000, () => {
-    console.log("engine started...");
+    console.log("Server is running on port 3000");
 });
+
+
+// Endpoint to list all users
+app.get('/users', (req, res) => {
+    // Map user data to include only required fields
+    const userList = users.map(user => ({ id: user.id, name: user.name, email: user.email }));
+    
+    res.json(userList); // Send the user list as JSON response
+  });
+  
+  // Start the server
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
