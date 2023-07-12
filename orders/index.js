4f6fba3d2b3a4062285a682b98f5bd14f20ed80a
sanchopedro <pedro_sancho@yahoo.com.br>
2023-07-12 19:53:58 +0100

    Fix: check before update

--- orders/index.js
+++ orders/index.js
@@ -9,15 +9,19 @@ router.put("/:id", (req, res) => {
   //order exists
   if (orderIx === -1) return res.status(404).json({ error: "order not found" });
 
-  //order update
-  orders[orderIx].status = req.body.status;
-
+  //body input
+  bodyStatus = req.body.status;
+  
   const possibleStatus = ["created", "preparing", "shipped", "delivered"];
-  if (!possibleStatus.includes(orders[orderIx].status)) {
-    return res.status(400).json({ error: "Invalid body" });
-  }
-  //result
-  res.status(200).json(orders);
+  
+  if (!possibleStatus.includes(bodyStatus)) {
+      return res.status(400).json({ error: "Invalid body" });
+    }
+// Update the status
+const previousStatus = orders[orderIx].status;
+orders[orderIx].status = bodyStatus;
+
+res.status(200).json({ previousStatus, newStatus: orders[orderIx].status });
 });
 
 module.exports = router;
