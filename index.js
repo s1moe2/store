33f6032b0f7cd8a84c15532ac0507f36adc2d219
Catarina Mendes <127792100+CatarinaFilipaMendes@users.noreply.github.com>
2023-07-12 19:40:08 +0100

    issue6

--- index.js
+++ index.js
@@ -5,37 +5,37 @@ const { logger } = require("./middleware");
 app.use(logger);
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: "internal error" });
+  console.error(err);
+  res.status(500).json({ error: "internal error" });
 });
 
 app.get("/users", (req, res) => {
-    const users = db.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-      };
-    });
-
-    res.json(users);
-    console.log(users);
+  const users = db.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
   });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  res.json(users);
+  console.log(users);
 });
 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
-// Endpoint to list all users
-app.get('/users', (req, res) => {
-    // Map user data to include only required fields
-    const userList = users.map(user => ({ id: user.id, name: user.name, email: user.email }));
-    
-    res.json(userList); // Send the user list as JSON response
-  });
-  
-  // Start the server
-  app.listen(3000, () => {
-    console.log('Server started on port 3000');
-  });
+app.get("/users", (req, res) => {
+  const userList = users.map((user) => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  }));
+
+  res.json(userList);
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
--- orders/index.js
+++ orders/index.js
@@ -1,1 +1,61 @@
-module.exports = {}
+const express = require("express");
+const { orders, products } = require("../db");
+const app = express();
+
+let orders = [];
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const { userId, products } = req.body;
+
+  // Gerar um unico ID
+  const orderId = generateOrderId();
+
+  const price = calculateTotalPrice(products);
+
+  const dateTime = new Date();
+
+  const status = "placed"; // Assuming "placed" is a valid status
+
+  // criar o objeto order
+  const order = {
+    userId,
+    products,
+    price,
+    dateTime,
+    status,
+  };
+
+  orders.push(order);
+
+  // enviar resposta
+  res.status(200).json(order);
+});
+
+// funcao calcular total produto
+function calculateTotalPrice(products) {
+  let totalPrice = 0;
+  for (let i = 0; i < products.length; i++) {
+    totalPrice += products[i].price;
+  }
+  return totalPrice;
+}
+
+// Gerar um id aleatorio
+function generateOrderId() {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let orderId = "";
+  for (let i = 0; i < 10; i++) {
+    orderId += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return orderId;
+}
+
+// Start servidor
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+module.exports = app; // exportar app
