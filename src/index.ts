4b19632ff09076ea1dfe9e759cc0a9c3fc842e32
Gerardo Lima <gerardo.lima@gmail.com>
2023-07-19 19:58:28 +0100

    refactor(index): migrate to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-const express = require("express");
+import express, { NextFunction, Request, Response } from "express";
+import { logger, auth } from "./middleware";
+import users from "./users";
+import products from "./products";
+import orders from "./orders";
+import authRoutes from "./auth";
+import * as config from "./config";
 const app = express();
-const { logger, auth } = require("./middleware");
-const users = require("./users");
-const products = require("./products");
-const orders = require("./orders");
-const authRoutes = require("./auth").default;
-const config = require("./config");
 
 app.use(express.json());
 app.use(logger);
@@ -18,8 +18,7 @@ app.use("/products", products);
 app.use("/users", users);
 app.use("/orders", orders);
 
-/* eslint-disable no-unused-vars*/
-app.use((err, req, res, next) => {
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction): void => {
   console.error(err);
   res.status(500).json({ error: "internal error" });
 });
